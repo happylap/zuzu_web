c095d18d3d7e8023330c55dcf0eb218eb445e2ec
ted-lee <alight.lee@gmail.com>
2016-04-28 21:21:28 +0800

    fix log

--- zuzu_notify/zuzuNotify/zuzu_solr.py
+++ zuzu_notify/zuzuNotify/zuzu_solr.py
@@ -138,8 +138,6 @@ class AsyncSolrClient(object):
         sort = ["price asc, post_time desc"]
         results = await self.query(query=q, filters=filters,columns=columns,sort=sort,start=0,rows=self.NOTIFY_ITEMS_LIMIT)
 
-        self.logger.info("found notify_items for "+notifier.user_id)
-
         notify_items = results.docs
 
         if notify_items is None or len(notify_items) < 1:
@@ -147,7 +145,7 @@ class AsyncSolrClient(object):
             notifier.last_notify_time = self.current_notify_time
             return notify_items
         else:
-            self.logger.info("found "+ len(notify_items )+" new notify items for "+notifier.user_id)
+            self.logger.info("found "+ str(len(notify_items))+" new notify items for "+notifier.user_id)
 
         self.logger.info("query criteria for "+notifier.user_id+": query=" + query["query"] + ", filters="+filters)
 
